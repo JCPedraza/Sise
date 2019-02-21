16f14790ec314c019058c82461024b39d396e23a
JCPedraza <juancarlos.jcuzu@gmail.com>
2019-02-21 09:26:56 -0600

    Acomodo del controlador, nueva vista

---
+++ application/views/templates/panel/nuevo_programa.php
@@ -0,0 +1,56 @@
+<!-- start: Content -->
+            <div id="content">
+                <div class="panel box-shadow-none content-header">
+                  <div class="panel-body">
+                    <div class="col-md-12">
+                        <h3 class="animated fadeInLeft">Formulario</h3>
+                        <p class="animated fadeInDown"> Nuevo Programa
+                        </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-element">
+                  <div class="col-md-12 padding-0">
+                    <div class="col-md-8">
+                      <div class="panel form-element-padding">
+                        <div class="panel-heading">
+                         <h4>Datos del Programa</h4>
+                        </div>
+                        <?php
+                             $atributos = array('class'=>'form-horizontal');
+                             echo form_open('sise/registro_nuevo_programa/',$atributos);
+                          ?>
+                         <div class="panel-body" style="padding-bottom:30px;">
+                          <div class="col-md-12">
+                            <div class="form-group"><label class="col-sm-2 control-label text-right" >Nombre del Programa</label>
+                              <div class="col-sm-10"><input type="text" minlength="15" maxlength="100" class="form-control android" name="nom_pro"></div>
+                            </div>
+                            <div class="form-group"><label class="col-sm-2 control-label text-right" >Descripción</label>
+                              <div class="col-sm-10"><input type="text" minlength="12" maxlength="150" class="form-control android" name="des_pro"></div>
+                            </div>
+                          </div>
+                          <div class="col-md-6" style="margin-top:5px;">
+                                   <button type="submit" name="formulario" class="btn ripple-infinite btn-round btn-warning">
+                                    <div>
+                                      <span>Guardar Cambios</span>
+                                    </div>
+                                  </button>
+                              </div>
+                        </div>
+                      </form>
+                      </div>
+                      </div>
+                          <div class="col-md-4 padding-0">
+                              <div class="panel bg-light-blue">
+                                <div class="panel-body text-white">
+                                   <p class="animated fadeInUp quote">Verifique los campos esten correctos</p>
+                                </div>
+                              </div>
+                            </div>
+                   
+                    
+                  
+                
+              </div>
+            </div>
+          <!-- end: content -->
